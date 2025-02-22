95e99337f7fec68a8855ab471c0778a762486f5b
Tom.L <tom.louveau@edu.devinci.fr>
2025-02-22 17:21:50 +0100

    ADD webviewpanel && esprima

--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,36 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+    let disposable = vscode.commands.registerCommand('codeMetrics.showMetrics', () => {
+        const panel = vscode.window.createWebviewPanel(
+            'codeMetrics',
+            'Code Metrics',
+            vscode.ViewColumn.One,
+            { enableScripts: true }
+        );
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "code-metrics-insight" is now active!');
+        const editor = vscode.window.activeTextEditor;
+        if (!editor) {
+            panel.webview.html = `<h1>No file open</h1>`;
+            return;
+        }
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('code-metrics-insight.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from code-metrics-insight!');
-	});
+        const code = editor.document.getText();
+        const lines = code.split('\n').length;
+        const words = code.split(/\s+/).length;
 
-	context.subscriptions.push(disposable);
+        panel.webview.html = `
+            <html>
+                <body>
+                    <h2>📊 Code Metrics</h2>
+                    <p>Lines: <strong>${lines}</strong></p>
+                    <p>Words: <strong>${words}</strong></p>
+                </body>
+            </html>
+        `;
+    });
+
+    context.subscriptions.push(disposable);
 }
 
-// This method is called when your extension is deactivated
 export function deactivate() {}
